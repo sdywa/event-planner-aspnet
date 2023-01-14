ff7b68a271687e6b6f6968fb707b7fe14714cb1a
sdywa <aleliger@protonmail.ch>
2023-01-14 17:58:36 +0300

    make a learn page

--- event-planner-front/src/pages/learn/Learn.tsx
+++ event-planner-front/src/pages/learn/Learn.tsx
@@ -1,10 +1,21 @@
 import { FC } from "react";
+import "./Learn.css";
 
 const Learn: FC = () => {
     return (
-        <div>
-            learn
-        </div>
+        <section className="section-intro">
+            <div className="intro-inner">
+                <div className="intro-image-box">
+                    <img src={require("../../assets/img/intro.png")} alt="Интро" />
+                </div>
+                <div className="intro-text-box">
+                    <h1 className="heading heading--primary">Простота организации мероприятий</h1>
+                    <p className="intro-description">
+                        Текст текст текст Текст текст текст Текст текст текст Текст текст текст Текст текст текст Текст текст текст Текст текст текст
+                    </p>
+                </div>
+            </div>
+        </section>
     );
 }
 
