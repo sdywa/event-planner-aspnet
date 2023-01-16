973c72a3057adcf7f7b50bf09c4ff25f2cacc478
sdywa <aleliger@protonmail.ch>
2023-01-16 17:32:56 +0300

    implement the sign up page

--- event-planner-front/src/pages/signup/Signup.tsx
+++ event-planner-front/src/pages/signup/Signup.tsx
@@ -1,9 +1,15 @@
 import { FC } from "react";
+import { AuthForm } from "../../components/UI/auth-form/AuthForm";
+import { Input } from "../../components/UI/input/Input";
 
 export const Signup: FC = () => {
     return (
-        <div>
-            signup
-        </div>
+        <AuthForm selectedTabIndex={1}>
+            <Input type="text" name="name" label="Ваше имя" autocomplete="off" />
+            <Input type="text" name="surname" label="Ваша фамилия" autocomplete="off" />
+            <Input type="text" name="email" label="Email" autocomplete="off" />
+            <Input type="password" name="password" label="Пароль" autocomplete="off" />
+            <Input type="password" name="password" label="Подтвердите пароль" autocomplete="off" />
+        </AuthForm>
     );
 }
