ad44a3b6d409cffc71477e0904a19420fd63f65b
sdywa <aleliger@protonmail.ch>
2023-01-18 21:34:21 +0300

    add a useInputStatus

---
+++ event-planner-front/src/hooks/useInputStatus.ts
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { IFormInputData, IFieldStatus } from "../types";
+import useInput from "./useInput";
+
+const useInputStatus = (
+    name: string,
+    data: IFormInputData,
+    callBack: (name: string, value: IFieldStatus) => void) => {
+    const {error, isDirty, removeDirty, ...props} = useInput("", data.validation);
+
+    useEffect(() => {
+        callBack(name, {
+            name: name,
+            hasError: Boolean(error),
+            isDirty: isDirty,
+            removeDirty: removeDirty
+        });
+    }, [error, isDirty]);
+
+    return {error, isDirty, ...data, ...props};
+}
+
+export default useInputStatus;
