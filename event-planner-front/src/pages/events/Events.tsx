be5e69083c5fe0d17c8eb8939da7f1df4be3540c
sdywa <aleliger@protonmail.ch>
2023-01-22 21:49:37 +0300

    make a raw events page

--- event-planner-front/src/pages/events/Events.tsx
+++ event-planner-front/src/pages/events/Events.tsx
@@ -1,9 +1,63 @@
 import { FC } from "react";
+import { PageLayout } from "../../components/layouts/page-layout/PageLayout";
+import { EventTile } from "../../components/UI/event-tile/EventTile";
+import "./Events.css";
 
 export const Events: FC = () => {
+    const events = [{
+        id: 1,
+        title: "Заголовок",
+        coverUrl: "",
+        description: "Описание мероприятия описание описание описание описание",
+        category: "Бизнес",
+        type: "Offline",
+        date: "12 декабря",
+        location: "г. Москва",
+        minPrice: 0,
+        isFavorite: false
+    },
+    {
+        id: 2,
+        title: "Очень очень длинный заголовок который не помещается",
+        coverUrl: "",
+        description: "Очень длинное название которое тоже не помещается в этот маленький блок что я не знаю что с ним делать",
+        category: "Искусство и культура",
+        type: "Offline",
+        date: "12 сентября, 2024 г.",
+        location: "г. Кременчуг-Константиновское",
+        minPrice: 99999,
+        isFavorite: true
+    },
+    {
+        id: 3,
+        title: "Заголовок",
+        coverUrl: "",
+        description: "Описание мероприятия описание описание описание описание",
+        category: "Бизнес",
+        type: "Online",
+        date: "12 декабря",
+        minPrice: 0,
+        isFavorite: false
+    },
+    {
+        id: 4,
+        title: "Заголовок",
+        coverUrl: "",
+        description: "Описание мероприятия описание описание описание описание",
+        category: "Бизнес",
+        type: "Online",
+        date: "12 декабря",
+        minPrice: 0,
+        isFavorite: false
+    }];
+
     return (
-        <div>
-            events
-        </div>
+        <PageLayout title="Мероприятия">
+            <div className="events-content grid grid--3-cols">
+                {
+                    events.map((v, i) => <EventTile key={v.id} {...v} />)
+                }
+            </div>
+        </PageLayout>
     );
 }
