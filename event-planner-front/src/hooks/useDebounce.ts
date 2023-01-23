118fa4a21eb7920b7672aee9c4eef8f8f72656a7
sdywa <aleliger@protonmail.ch>
2023-01-23 14:40:09 +0300

    add a useDebounce hook

---
+++ event-planner-front/src/hooks/useDebounce.ts
@@ -0,0 +1,16 @@
+import { useState, useCallback } from "react";
+
+function useDebounce<T>(callback: (args: T) => void, delay: number) {
+    const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+
+    const debouncedCallback = useCallback((args: T) => {
+        if (timer)
+            clearTimeout(timer);
+        
+        setTimer(setTimeout(() => callback(args), delay));
+    }, [callback, delay, timer]);
+
+    return debouncedCallback;
+}
+
+export default useDebounce;
