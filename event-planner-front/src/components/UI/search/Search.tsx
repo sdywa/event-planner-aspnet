acac5fc6def814dc52b4d37c3eb52f841e961ef5
sdywa <aleliger@protonmail.ch>
2023-01-23 14:41:23 +0300

    create search component

---
+++ event-planner-front/src/components/UI/search/Search.tsx
@@ -0,0 +1,28 @@
+import { FC, useState } from "react";
+import useDebounce from "../../../hooks/useDebounce";
+import { Input } from "../input/Input";
+import "./Search.css";
+
+interface ISearchProps {
+    searchUrl: string;
+};
+
+export const Search: FC<ISearchProps> = ({searchUrl: string}) => {
+    const [value, setValue] = useState("");
+    const debouncedSearch = useDebounce<string>(search, 500);
+
+    function search(query: string) {
+        console.log(`searching... ${query}`);
+    }
+
+    function onChange (e: React.ChangeEvent<HTMLInputElement>) {
+        setValue(e.target.value);
+        debouncedSearch(e.target.value);
+    }
+
+    return (
+        <div className="search input-box-inner">
+            <Input type="text" name="search" value={value} onChange={onChange} placeholder="Поиск"/>
+        </div>
+    );
+}
