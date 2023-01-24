50045535d7ee58d22908c48a19cb8b7e6187feab
sdywa <aleliger@protonmail.ch>
2023-01-24 20:40:21 +0300

    setup tailwindcss

---
+++ event-planner-front/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+    mode: "jit",
+    content: [
+        "./src/**/*.{ts,tsx}",
+        "./public/**/*.html"
+    ],
+    theme: {
+        extend: {
+        colors: {
+            white: "#FFFFFF",
+            backgroundWhite: "#F1F6F9",
+            black: "#263147",
+            lightgray: "#b4bbca",
+            gray: "#858fa4",
+            darkgray: "#656e82",
+            green: "#00B94F",
+            red: "#E02B4C",
+            yellow: "#EBB428" 
+        },
+        fontFamily: {
+            ubuntu: ["Ubuntu", "sans-serif"],
+            roboto: ["Roboto", "sans-serif"],
+        }
+        },
+    },
+    plugins: [],
+}
