2f15c5b9ee7ec625f15c8efe99384b0ffb9d2024
sdywa <aleliger@protonmail.ch>
2023-01-28 10:33:46 +0300

    fix styles

--- event-planner-front/src/pages/event/Event.tsx
+++ event-planner-front/src/pages/event/Event.tsx
@@ -5,7 +5,7 @@ import { Bookmark } from "../../components/UI/bookmark/Bookmark";
 import { Button, ButtonStyles } from "../../components/UI/button/Button";
 import { Location } from "../../components/UI/events/location/Location";
 import { WithIcon } from "../../components/UI/with-icon/WithIcon";
-import { IEvent, IExtendedEvent } from "../../types";
+import { IExtendedEvent } from "../../types";
 
 export const Event: FC = () => {
     // const params = useParams();
@@ -17,7 +17,7 @@ export const Event: FC = () => {
         category: "Бизнес",
         type: "Offline",
         date: "Понедельник, 12 декабря",
-        location: "г. Москва, ...",
+        location: "г. Москва, очень длинный адрес который может не поместиться в этот маленький блок",
         minPrice: 0,
         isFavorite: false,
         creator: {
@@ -39,15 +39,15 @@ export const Event: FC = () => {
         <PageLayout title={event.title} isCentered={true} header={
             <Bookmark isFavorite={event.isFavorite} className={["text-lg"]} favoriteCallback={setFavorite} />
         }>
-            <div className="flex justify-center items-center gap-3 mt-2">
+            <div className="m-auto max-w-2xl flex flex-wrap justify-center items-center gap-3 mt-2">
                 <WithIcon icon={<i className="fa-solid fa-calendar"></i>}>
                     {event.date}
                 </WithIcon>
                 <Location type={event.type} location={event.location} />
             </div>
             <div className="flex justify-center items-center gap-4 py-4 relative">
-                <div className="flex flex-col justify-center items-start gap-4 text-xl">
-                    <div className="w-[44rem] h-[25rem] relative bg-lightgray rounded-md text-xl">
+                <div className="flex flex-col justify-center items-start gap-4">
+                    <div className="w-[44rem] h-[25rem] relative bg-lightgray rounded-md">
                         {
                             event.coverUrl && <img src={event.coverUrl} alt={event.title} />
                         }
