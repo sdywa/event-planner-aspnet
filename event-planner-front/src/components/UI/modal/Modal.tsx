e0b66338b2ce2c69929ac292ae75a0c55326e751
sdywa <aleliger@protonmail.ch>
2023-05-14 11:48:26 +0300

    fix popup overlap

--- event-planner-front/src/components/UI/modal/Modal.tsx
+++ event-planner-front/src/components/UI/modal/Modal.tsx
@@ -16,7 +16,7 @@ export const Modal: FC<IModalProps> = ({active, setActive, children}) => {
     }, [active]);
 
     return (
-        <div className={clsx("h-screen w-screen bg-[rgba(0,0,0,0.4)] fixed inset-0 z-50 flex justify-center items-center ease-in duration-100", active ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none")} onClick={() => setActive(false)}>
+        <div className={clsx("h-screen w-screen bg-[rgba(0,0,0,0.4)] fixed inset-0 z-[99999] flex justify-center items-center ease-in duration-100", active ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none")} onClick={() => setActive(false)}>
             <div className={clsx("py-6 px-12 bg-white w-fit h-fit rounded-xl shadow-xl ease-in duration-75", active ? "translate-y-0 scale-100" : "-translate-y-1 scale-95")} onClick={(e) => e.stopPropagation()}>
                 {children}
             </div>
